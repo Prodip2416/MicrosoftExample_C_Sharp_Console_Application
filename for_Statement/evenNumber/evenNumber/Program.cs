7f26a3e8285d9045000da2267e1d1a60cb8838f1
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-01-07 06:44:35 +0600

    C#

---
+++ for_Statement/evenNumber/evenNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0;i<=100;i=i+2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
