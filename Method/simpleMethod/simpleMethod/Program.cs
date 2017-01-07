4a1b76de3f17c0fcf463c64e037707be55cc267c
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-01-07 06:46:12 +0600

    C#

---
+++ Method/simpleMethod/simpleMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reversestring();
+            Console.ReadLine();
+        }
+        static void Reversestring()
+        {
+            string message = "Helllo World!";
+            char[] messageArray = message.ToCharArray();
+            Array.Reverse(messageArray);
+            foreach(char item in messageArray)
+            {
+                Console.Write(item);
+            }
+        }
+    }
+}
