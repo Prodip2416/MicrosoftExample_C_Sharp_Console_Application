ee65a072ddb8869eb5ba3685711c181c975b27a7
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-01-07 06:40:00 +0600

    C#

---
+++ Class/ObjectLifeTime/ObjectLifeTime/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectLifeTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car myCar = new Car();
+            /*
+          myCar.Make = "Oldmobile";
+          myCar.Model = "Cutles Supreme";
+          myCar.Year = 1886;
+          myCar.Color = "Silver";
+             */
+
+            Car myThirdCar = new Car("Ford", "Escape", 2005, "White");
+            Car myOldCar;
+            myOldCar = myCar;
+
+            Console.WriteLine("{0} {1} {2} {3}",
+                myOldCar.Make,
+                myOldCar.Model,
+                myOldCar.Year,
+                myOldCar.Color);
+
+            myOldCar.Model = "98";
+
+            Console.WriteLine("{0} {1} {2} {3}",
+              myOldCar.Make,
+              myOldCar.Model,
+              myOldCar.Year,
+              myOldCar.Color);
+
+            Console.ReadLine();
+        }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        
+        public Car()
+        {
+            this.Make = "Nissan";
+        }
+
+        public Car(string make,string model,int year,string color)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Color = color;
+        }
+        
+
+    }
+}
---
+++ Class/simpleClass/simpleClass/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Oldsmobiles";
+            myCar.Model = "Cutles Suprem";
+            myCar.Year = 1986;
+            myCar.Color = "Silver";
+
+            Console.WriteLine("{0} {1} {2} {3}",
+                myCar.Make,
+                myCar.Model,
+                myCar.Year,
+                myCar.Color);
+
+            // decimal value = DetermineMarkletValue(myCar);
+            // Console.WriteLine("{0:C}",value);
+
+            Console.WriteLine("{0:C}",myCar.DetermineMarketValue());
+            Console.ReadLine();
+        }
+        private static decimal DetermineMarkletValue(Car car)
+        {
+            decimal carValue = 100.0M;
+
+            return carValue;
+        } 
+    }
+    class Car
+    {
+        public string Model { get; set; }
+        public string Make { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public decimal DetermineMarketValue()
+        {
+            decimal Carvalue;
+            if (Year > 1990)
+                Carvalue = 10000;
+            else
+                Carvalue = 1000;
+
+            return Carvalue;
+        }
+
+    }
+}
