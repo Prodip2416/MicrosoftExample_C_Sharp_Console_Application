b2d2424b8ba77cf8879bb3f1763bebf1a6d7b1b0
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-01-07 06:47:18 +0600

    C#

---
+++ Scope/scope/scope/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scope
+{
+    class Program
+    {
+        private static string k = "";
+        static void Main(string[] args)
+        {
+            string j = "";
+            for (int i=0;i<10;i++)
+            {
+                j = i.ToString();
+                k = i.ToString();
+                Console.WriteLine(j);
+            }
+            Console.WriteLine("Outside of the for: " +j);
+            Console.WriteLine("Outside of the for: " + k);
+
+            HelperMethod();
+
+            Car myCar = new Car();
+            myCar.DoSomething();
+            Console.ReadLine();
+        }
+        static void HelperMethod()
+        {
+            Console.WriteLine("Value of k from the HelperMethod: " + k);
+        }
+    }
+    class Car
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine(HelperMethod());
+        }
+        private string HelperMethod()
+        {
+            return "Hello World!";
+        }
+    }
+}
