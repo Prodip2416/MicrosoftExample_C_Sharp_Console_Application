af06d1961cdc2673c8d05ee11bf0a67fdc666e74
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-01-07 06:51:49 +0600

    C#

---
+++ Handling/handlingException/handlingException/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace handlingException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           try
+            {
+                string content = File.ReadAllText(@"C:\Lession\example.txt");
+                Console.WriteLine(content);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("There was a problem.");
+                Console.WriteLine(ex.Message);
+            }
+                 Console.ReadLine();
+        }
+    }
+}
