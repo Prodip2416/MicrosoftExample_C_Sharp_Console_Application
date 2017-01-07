a849fe57cd0896386ae6cb701c247ee528aa0cb6
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-01-07 06:46:32 +0600

    C#

---
+++ NameSpace/assembliesAndNamespace/assembliesAndNamespace/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace assembliesAndNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient client = new WebClient();
+            string replay = client.DownloadString("http://msdn.microsoft.com");
+
+            Console.WriteLine(replay);
+            File.WriteAllText(@"E:\Programming With DOT NET\C# Console Application\Microsoft Tutorial\NameSpace\WriteText.txt", replay);
+            Console.ReadLine();
+        }
+    }
+}
