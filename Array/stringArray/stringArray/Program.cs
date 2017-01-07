11f69c58b2504f35116c75f860777dc795fe3001
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-01-07 06:38:03 +0600

    C#

---
+++ Array/stringArray/stringArray/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[] { "prodip", "Tuhin", "Fatema" };
+
+            for(int i=0;i<names.Length;i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
